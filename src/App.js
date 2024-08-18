12a78dadf8813a1faa43ba934546d8ff02d17342
Diego <ale.rgdap@gmail.com>
2024-08-18 15:51:05 -0600

    random color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordion from './components/accordion';
+import Random from './components/random';
 
 function App() {
   return (
     <div className="App">
         <Accordion></Accordion>
+        <Random></Random>
     </div>
   );
 }
--- src/components/random/index.jsx
+++ src/components/random/index.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react"
+
 export default function Random(){
+
+    const [type, setType] = useState('hex')
+    const [color, setColor] = useState('#000000')
+
+    function randomColor(length){
+        return Math.floor(Math.random() * length)
+    }
+
+    function createHexColor(){
+        const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+        let hexColor ='#'
+        for(let i = 0; i<6;i++){
+            hexColor += hex[randomColor(hex.length)]
+        }
+        setColor(hexColor)
+    }
+
+    function createRgbColor(){
+        const r = randomColor(256)
+        const g = randomColor(256)
+        const b = randomColor(256)
+
+        setColor(`rgb(${r}, ${g}, ${b})`)
+    }
+
+    useEffect(()=>{
+        if(type=== 'rgb'){
+            createRgbColor()
+        }
+        else{
+            createHexColor()
+        }
+    }, [type])
+
     return(
-        <h1>hello</h1>
+        <div style={{
+            width: "100vw",
+            height: "100vh",
+            background: color
+        }} 
+        className="container">
+            <button onClick={()=>setType('hex')}>Clear Hex Color</button>
+            <button onClick={()=>setType('rgb')}>Clear RGB Color</button>
+            <button onClick={type === 'hex' ? createHexColor : createRgbColor}>Generate Random Color</button>
+            <div>
+                {color}
+            </div>
+        </div>
     )
 }
